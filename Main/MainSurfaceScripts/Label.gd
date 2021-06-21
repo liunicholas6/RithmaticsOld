813ce856cb77c96d3a16bfac37f9ebbfcceda34f
CrypticSpren <86247452+CrypticSpren@users.noreply.github.com>
2021-06-21 11:48:34 -0400

    Update Label.gd

--- Main/MainSurfaceScripts/Label.gd
+++ Main/MainSurfaceScripts/Label.gd
@@ -1,16 +1,7 @@
 extends Label
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-
+	pass
 
 func _process(_delta):
 	visible = Singleton.get_Debug()
