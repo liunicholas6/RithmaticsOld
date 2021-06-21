35ce2ba555ebf909ce7c563fe9ee25dfc0f7ec81
CrypticSpren <86247452+CrypticSpren@users.noreply.github.com>
2021-06-21 12:05:02 -0400

    Delete Main directory

--- Main/MainSurfaceScripts/BoundsHitbox.gd
+++
@@ -1,5 +0,0 @@
-extends CollisionShape2D
-
-func _ready():
-	position = get_viewport_rect().end/2
-	shape.extents = position+Vector2(50,50)
--- Main/MainSurfaceScripts/Label.gd
+++
@@ -1,7 +0,0 @@
-extends Label
-
-func _ready():
-	pass
-
-func _process(_delta):
-	visible = Singleton.get_Debug()
